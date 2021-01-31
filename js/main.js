60f258514599e79271555d480e2fcbfbf46f0e2b
Kronapsys <rsxvii@gmail.com>
2021-01-31 01:00:15 +0100

    Agregada funcionalidad a los botones y pagina cv.html creada

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function ToGithub(){
+    window.open("https://github.com/Kronapsys", "_blank");
+}
+
+function ToLinkedin(){
+    window.open("https://www.linkedin.com/in/roberto-s%C3%A1nchez-moyano-3119b0124/", "_blank");
+}
